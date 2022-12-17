8a6b6f571d1df81a97c3326706206f745e067c6e
Stas-0k <brickbybrick15@gmail.com>
2022-12-18 00:21:06 +0200

    commit
    commit

--- src/js/popular_films.js
+++ src/js/popular_films.js
@@ -1,41 +1,63 @@
-const cardList = document.querySelector('.card')
-const URLmain= 'https://api.themoviedb.org/3/trending/movie/day/94xxm5701CzOdJdUEdIuwqZaowx.jpg?api_key=177f83f5259c7f846e561f4715bd03a4'
-const URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=177f83f5259c7f846e561f4715bd03a4';
+const cardList = document.querySelector('.card');
+const URL =
+  'https://api.themoviedb.org/3/trending/movie/day?api_key=177f83f5259c7f846e561f4715bd03a4';
 
- function fetchPopular() { 
-     return fetch(URL).then(responce => { 
-        if (!responce.ok) { 
-            throw new Error(responce.statusText); 
-        }  return responce.json()
-     })
+const URLgenre = 'https://api.themoviedb.org/3/genre/movie/list?api_key=177f83f5259c7f846e561f4715bd03a4&language=en-US';
+
+function fetchGenres() {
+   return fetch(URLgenre).then(responce => {
+    if (!responce.ok) {
+      throw new Error(responce.statusText);
+    }
+   return responce.json();
+   })
 }
 
-function renderList(films) { 
-    films.results.map(film => cardList.innerHTML+=`<li class="card__item item">
+let genresArr = [];
+fetchGenres().then(genre => {genresArr= genre.genres})
+ console.log(genresArr)
+
+
+function fetchPopular() {
+  return fetch(URL).then(responce => {
+    if (!responce.ok) {
+      throw new Error(responce.statusText);
+    }
+    return responce.json();
+  });
+}
+
+function renderList(films) {
+  films.results.map(film => {
+    let year = new Date(film.release_date);
+    let yearRelease = year.getFullYear();
+    //console.log(film.genre_ids)
+    return (cardList.innerHTML += `<li class="card__item item">
                   <a class="card__link link" href="#" data-modal-open>
                     <div class="card__thumb">
                       <img                       
                         sizes="(min-width: 1200px) 370px, (min-width: 768px) 354px, (max-width: 767px) 450px, 100vw"
-                        src=""
+                        src='https://www.themoviedb.org/t/p/w500/${film.poster_path}'
                         alt="tehnocryak"
                         width="100%"
                       />
                     </div>
                     <div class="card__meta">
                       <h2 class="card__heading">${film.original_title}</h2>
-                      <p class="card__lead">genre | year</p>
+                      <p class="card__lead">genre | ${yearRelease}</p>
                     </div>
                   </a>
-                </li>`)
+                </li>`);
+  });
 }
 
-function markupPopular() { 
-    fetchPopular().then(films =>renderList(films)).catch(error=>console.log(error))
+function markupPopular() {
+  fetchPopular()
+    .then(films => renderList(films))
+    .catch(error => console.log(error));
 }
 
-markupPopular()
- 
-console.log(cardList)
+markupPopular();
 
 
 
