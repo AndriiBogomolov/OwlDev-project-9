3a90d448c232f6e7d0dc8f3d4f24c5e23e3ed2a5
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2022-12-18 00:57:29 +0200

    Adding refs for getting the full info about film

---
+++ src/js/refs.js
@@ -0,0 +1,12 @@
+export const refs = {
+	film: document.querySelector("#id"),
+	modalOpen: document.querySelector("[data-modal-open]"),
+	modalFilmImg: document.querySelector(".modal__img"),
+	modalFilmTitle: document.querySelector(".modal__title"),
+	modalFilmVote: document.querySelector(".vote"),
+	modalFilmVotes: document.querySelector(".votes"),
+	modalFilmPopularity: document.querySelector(".popularity"),
+	modalFilmOrigTitle: document.querySelector(".orig-title"),
+	modalFilmGenre: document.querySelector(".genre"),
+	modalFilmDescription: document.querySelector(".about__text"),
+};  
