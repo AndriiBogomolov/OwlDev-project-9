376f2156469b7399b29bcef71bcd105bf37c8260
AndriiBogomolov <97605054+AndriiBogomolov@users.noreply.github.com>
2022-12-18 20:15:48 +0000

    Deploying to gh-pages from @ AndriiBogomolov/OwlDev-project-9@8c0cf1fb315638c7bfd40b1b252dd55f22b8fae9 🚀

--- index.7e8937c6.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelectorAll("[data-film-open]"),closeModalBtn:document.querySelector("[data-film-close]"),modal:document.querySelector("[data-film]")};function o(){document.body.classList.toggle("film-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.forEach((function(e){e.addEventListener("click",o)})),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.7e8937c6.js.map
