e5ba6bcd811d87b2bd774211fda55f8d5b43d415
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2022-12-18 19:38:08 +0100

    upd2

--- src/js/modal_film.js
+++ src/js/modal_film.js
@@ -1,18 +1,17 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelectorAll("[data-film-open]"),
-      closeModalBtn: document.querySelector("[data-film-close]"),
-      modal: document.querySelector("[data-film]"),
-    };
-  
-    refs.openModalBtn.forEach(btn => {btn.addEventListener("click", toggleModal)});
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      document.body.classList.toggle("film-open")
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-film-open]'),
+    closeModalBtn: document.querySelector('[data-film-close]'),
+    modal: document.querySelector('[data-film]'),
+  };
 
+  refs.openModalBtn.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  
+  function toggleModal() {
+    document.body.classList.toggle('film-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
