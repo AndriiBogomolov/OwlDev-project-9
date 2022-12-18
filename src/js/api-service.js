0e8e65474c79d32211a48b8f9bdd54dbe1b073a1
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2022-12-18 20:17:03 +0300

    summary

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,3 +1,51 @@
+// import axios from 'axios';
+
+
+
+// const API_KEY = 'e52c7d8699df589ec79fa44e6b7f6a0c';
+// const BASE_URL = 'https://api.themoviedb.org';
+
+
+// export default class FilmotekaApiService {
+//     constructor() {
+//         this.searchQuery = '';
+//         this.page = 1;
+//     }
+
+//     fetch() {
+//         const url = `${BASE_URL}3/search/movie?api_key=${API_KEY}&query=Jack + Reacher`;
+//         return url
+//             .get(url)
+//             .then(function (response) {
+//                 console.log(response.json)
+//                 return response.json();
+//             })
+//             .then(({ object }) => {
+//                 this.incrementPage();
+//                 return { object };
+//             })
+//             .catch(error => console.log(error));
+
+//     }
+
+//     incrementPage() {
+//         this.page += 1;
+//     }
+
+//     resetPage() {
+//         this.page = 1;
+//     }
+
+//     get query() {
+//         return this.searchQuery;
+//     }
+
+//     set query(newQuery) {
+//         this.searchQuery = newQuery;
+//     }
+
+// }
+
 const API_KEY = 'e52c7d8699df589ec79fa44e6b7f6a0c';
 const BASE_URL = 'https://api.themoviedb.org/';
 
@@ -39,3 +87,13 @@ export default class ApiServise {
         this.searchQuery.innerHTML = '';
     }
 }
+
+
+
+
+let currentPage = 1;
+let totalPages;
+const pageRange = 2;
+
+init();
+refs.searchForm.addEventListener('submit', onSearchForm);
