7d7ba1c41e18ad499495524a0b92545ea84edeb1
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2022-12-19 22:36:05 +0200

    Fetching full info about film

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+export const refs = {
+	modalFilmImg: document.querySelector(".modal__img"),
+	modalFilmTitle: document.querySelector(".modal-film__title"),
+	modalFilmVote: document.querySelector(".vote"),
+	modalFilmVotes: document.querySelector(".votes"),
+	modalFilmPopularity: document.querySelector(".popularity"),
+	modalFilmOrigTitle: document.querySelector(".orig-title"),
+	modalFilmGenre: document.querySelector(".genre"),
+	modalFilmDescription: document.querySelector(".about__text"),
+};
