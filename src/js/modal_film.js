ad622fd5cd69c619e7b016d2891b26fc3310f448
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2022-12-19 12:42:05 +0200

    ft-17 without checking

--- src/js/modal_film.js
+++ src/js/modal_film.js
@@ -1,15 +1,48 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelectorAll("[data-film-open]"),
-      closeModalBtn: document.querySelector("[data-film-close]"),
-      modal: document.querySelector("[data-film]"),
-    };
-  
-    refs.openModalBtn.forEach(btn => {btn.addEventListener("click", toggleModal)});
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+import {Notify} from 'notiflix';
+
+(() => {  
+    refs.modalOpen.forEach(btn => {btn.addEventListener("click", toggleModal)});
+    refs.modalClose.addEventListener("click", toggleModal);
   
     function toggleModal() {
       document.body.classList.toggle("film-open")
       refs.modal.classList.toggle("is-hidden");
     }
-  })();
+})();
+
+const BASE_URL = "https://api.themoviedb.org/3/movie";
+const API_KEY = "e52c7d8699df589ec79fa44e6b7f6a0c";
+
+refs.modalOpen.addEventListener("click", onModalOpen);
+
+function onModalOpen(evt) {
+  const data = requestFullInfo().then((data) => fillingMurkup(data));
+}
+
+function requestFullInfo() {
+  const filmId = refs.film.getAttribute(id);
+  return fetch(`${BASE_URL}/${filmId}?api_key=${API_KEY}`)
+    .then((resp) => {
+      if (!resp.ok) {
+        throw new Error();
+      }
+      return resp.json();
+    })
+    .catch((er) =>
+      Notify.failure("Sorry, something goes wrong. Please, try again")
+    );
+}
+
+function fillingMurkup(obj) {
+  const genres = obj.genres.map((genre) => genre.name).join(", ");
+
+  refs.modalFilmImg.src = `https://image.tmdb.org/t/p/w500/${obj.poster_path}`;
+  refs.modalFilmImg.alt = `${obj.title} poster`;
+  refs.modalFilmTitle.textContent = obj.title;
+  refs.modalFilmVote.textContent = obj.vote_average;
+  refs.modalFilmVotes.textContent = obj.vote_count;
+  refs.modalFilmPopularity.textContent = obj.popularity;
+  refs.modalFilmOrigTitle.textContent = obj.original_title;
+  refs.modalFilmDescription.textContent = obj.overview;
+  refs.modalFilmGenre.textContent = genres;
+}
---
+++ src/js/refs.js
@@ -0,0 +1,14 @@
+const refs = {	
+	modalOpen: document.querySelector("[data-film-open]"),
+	modalClose: document.querySelector("[data-film-close]"),
+    modal: document.querySelector("[data-film]"),
+	film: document.querySelector("[id]"),
+	modalFilmImg: document.querySelector(".modal__img"),
+	modalFilmTitle: document.querySelector(".modal__title"),
+	modalFilmVote: document.querySelector(".vote"),
+	modalFilmVotes: document.querySelector(".votes"),
+	modalFilmPopularity: document.querySelector(".popularity"),
+	modalFilmOrigTitle: document.querySelector(".orig-title"),
+	modalFilmGenre: document.querySelector(".genre"),
+	modalFilmDescription: document.querySelector(".about__text"),
+};
